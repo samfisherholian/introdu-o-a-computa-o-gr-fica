598f6ea8acecdfc967f320dd2840362387d6f993
leilongchim <50149771+leilongchim@users.noreply.github.com>
2020-06-24 13:47:38 -0300

    modificação por marcus

--- icg/mygl.c
+++ icg/mygl.c
@@ -9,115 +9,86 @@
 // Um pixel é composto por 4 bytes
 typedef struct
 {
+    int x;
+    int y;
+}Pixel;
+Pixel p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12;
+typedef struct
+{
     int red;
     int green;
     int blue;
     int alpha;
  
 }RGBA;
-
-RGBA rgba1,rgba2, rgba;
-
-typedef struct
-{
-    int x;
-    int y;
-}Pixel;
-Pixel pixel,p1,p2,p3;
-
-double inter1;
-double inter2;
-double inter3;
-double inter4;
-
+RGBA cor1,cor2, corpixel;
+float inter1;
+float inter2;
+float inter3;
+float inter4;
 //
 // >>> Defina aqui as funções que você implementar <<< 
 //
 // Definição da função que chamará as funções implementadas pelo aluno
 void MyGlDraw(void) {
 
-//printPixels();
+printPixels();
 printLines();
-//printTriagle();
+printTriagle();
 }
-
-int pos(int x, int y){
-    int l = ((y*4)*IMAGE_WIDTH); //l é linha, 4 bytes por pixel
-    int c = (x*4);               //c é coluna, 4 bytes por pixel
-    int position = l + c;
-    return position;          
-  }
-
-  void putPixel(Pixel pixel, RGBA rgba){
-//teste para que não sejam pintados pixels indevidos na tela, limitando o desenho a apenas a area //disponível
- if((pixel.x>=0 && pixel.x<IMAGE_HEIGHT) && (pixel.y>=0 && pixel.y<IMAGE_WIDTH)){
+void putPixel(Pixel p, RGBA cor){
+    if((p.x>=0 && p.x<IMAGE_HEIGHT) && (p.y>=0 && p.y<IMAGE_WIDTH)){
     //cores para os pixels
-    fb_ptr[pos(pixel.x, pixel.y)+0] = rgba.red;
-    fb_ptr[pos(pixel.x, pixel.y)+1] = rgba.green;
-    fb_ptr[pos(pixel.x, pixel.y)+2] = rgba.blue;
-    fb_ptr[pos(pixel.x, pixel.y)+3] = rgba.alpha;
-    //cores para os pixels do ponto 1 de uma reta
-    fb_ptr[pos(p1.x, p1.y)+0] = rgba.red;
-    fb_ptr[pos(p1.x, p1.y)+1] = rgba.green;
-    fb_ptr[pos(p1.x, p1.y)+2] = rgba.blue;
-    fb_ptr[pos(p1.x, p1.y)+3] = rgba.alpha;
-    //cores para os pixels do ponto 2 de uma reta
-    fb_ptr[pos(p2.x, p2.y)+0] = rgba.red;
-    fb_ptr[pos(p2.x, p2.y)+1] = rgba.green;
-    fb_ptr[pos(p2.x, p2.y)+2] = rgba.blue;
-    fb_ptr[pos(p2.x, p2.y)+3] = rgba.alpha;
-    //cores para os pixels do ponto 3 de uma reta
-    fb_ptr[pos(p3.x, p3.y)+0] = rgba.red;
-    fb_ptr[pos(p3.x, p3.y)+1] = rgba.green;
-    fb_ptr[pos(p3.x, p3.y)+2] = rgba.blue;
-    fb_ptr[pos(p3.x, p3.y)+3] = rgba.alpha;
- }
+    fb_ptr[p.x*4 +p.y*4*IMAGE_WIDTH+0] = cor.red;
+    fb_ptr[p.x*4 +p.y*4*IMAGE_WIDTH+1] = cor.green;
+    fb_ptr[p.x*4 +p.y*4*IMAGE_WIDTH+2] = cor.blue;
+    fb_ptr[p.x*4 +p.y*4*IMAGE_WIDTH+3] = cor.alpha;
+    }
 }
-
 //Abaixo esta a funcao de acender pixels na tela com uma determinada
 // cor num lugar determinado
 void printPixels()
-  {/*
+  {
     // pixel da cor vermelha na posição coluna 1 e linha 100
-    pixel.x = 1;
-    pixel.y = 100;
-    rgba.red = 255;
-    rgba.green = 0;
-    rgba.blue = 0;
-    rgba.alpha = 255; 
-    putPixel(pixel, rgba); //chamada da funcao void putpixel
+    p1.x = 1;
+    p1.y = 100;
+    corpixel.red = 255;
+    corpixel.green = 0;
+    corpixel.blue = 0;
+    corpixel.alpha = 255; 
+    putPixel(p1,corpixel); //chamada da funcao void putpixel
 
     // pixel da cor verde na coluna 300 e linha 10
-    pixel.x = 300;
-    pixel.y = 10;
-    rgba.red = 0;
-    rgba.green = 255;
-    rgba.blue = 0;
-    rgba.alpha = 255; 
-    putPixel(pixel, rgba);
+    p2.x = 300;
+    p2.y = 10;
+    corpixel.red = 0;
+    corpixel.green = 255;
+    corpixel.blue = 0;
+    corpixel.alpha = 255; 
+    putPixel(p2,corpixel);
 
     //píxel da cor azul na coluna 250 e na linha 150
-    pixel.x = 250;
-    pixel.y = 150;
-    rgba.red = 0;
-    rgba.green = 0;
-    rgba.blue = 255;
-    rgba.alpha = 255; 
-    putPixel(pixel, rgba);
+    p3.x = 250;
+    p3.y = 150;
+    corpixel.red = 0;
+    corpixel.green = 0;
+    corpixel.blue = 255;
+    corpixel.alpha = 255; 
+    putPixel(p3,corpixel);
 
     //Afim de formar um desenho podemos colocar pixels onde quisermos
-    for(int px = 0;px <= 512;px+=+8){
-      for(int py = 452;py <= 512; py +=4){
+    for(int px = 0;px <= 512;px=px+8){
+      for(int py = 452;py <= 512; py = py+4){
         if (px == 512){
         px == 0;
         }
-    pixel.x = px;
-    pixel.y = py;
-    rgba.red = 191;
-    rgba.green = 187;
-    rgba.blue = 63;
-    rgba.alpha = 255; 
-    putPixel(pixel, rgba);
+    p4.x = px;
+    p4.y = py;
+    corpixel.red = 191;
+    corpixel.green = 187;
+    corpixel.blue = 63;
+    corpixel.alpha = 255; 
+    putPixel(p4,corpixel);
       }
     }
     for(int px = 5;px <= 512;px=px+8){
@@ -125,231 +96,221 @@ void printPixels()
         if (px == 512){
         px == 5;
         }
-    pixel.x = px;
-    pixel.y = py;
-    rgba.red = 255;
-    rgba.green = 255;
-    rgba.blue = 255;
-    rgba.alpha = 255; 
-    putPixel(pixel, rgba);
+    p5.x = px;
+    p5.y = py;
+    corpixel.red = 255;
+    corpixel.green = 255;
+    corpixel.blue = 255;
+    corpixel.alpha = 255; 
+    putPixel(p5,corpixel);
       }
     }
-    */
   }
    
 
 //funcao de desenhar uma linha
-void drawline(Pixel p1, Pixel p2, RGBA cor1, RGBA cor2){
-    Pixel A = p1, B = p2;
+void drawline(Pixel a, Pixel b, RGBA cor1, RGBA cor2){
+    Pixel A = a, B = b;
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0; //incrementos
     int dX = B.x - A.x;  //delta X = x2 - x(incicial);
     int dY = B.y - A.y;  //delta Y = y2 - y(inicial);
-    //armazenamento dos eixos
-    int eixoMaior, eixoMenor;   
-    int incrementoX0 = 0, incrementoY0 = 0, incrementoX1 = 0, incrementoY1 = 0, numerador;
-
+    int M, m;   
+    int N;
     //testes para analisar se os eixos estão crescendo ou decrescendo de acordo com o delta
     if (dX > 0){            
-        incrementoX0 = incrementoX1 = 1;
+        x1++;
+        x2++;
     }
-
     if (dX < 0){     
-        incrementoX0 = incrementoX1 = -1;
+        x1--;
+        x2--;
     }
-
     if (dY > 0){              
-        incrementoY0 = 1;
+        y1++;
     }
     if (dY < 0){     
-        incrementoY0 = -1 ;
+        y1--;
     }
 
     if (abs(dX) >= abs(dY)){     
-        eixoMaior = abs(dX);
-        eixoMenor = abs(dY);
+        M = abs(dX);
+        m = abs(dY);
     }else {
-        eixoMaior = abs(dY);
-        eixoMenor = abs(dX);
+        M = abs(dY);
+        m = abs(dX);
 
          if(dY > 0){         
-            incrementoY1 = 1;
+            y2 = 1;
         }
 
         if (dY < 0){    
-            incrementoY1 = -1;
+            y2 = -1;
         } 
-        incrementoX1 = 0;
+        x2 = 0;
     }
-    numerador = eixoMaior/2;
+    N = M/2;
+    inter1 = ((cor2.red) - (cor1.red)) / dX;
+    inter2 = ((cor2.blue) - (cor1.blue)) / dX;
+    inter3 = ((cor2.green) - (cor1.green)) / dX;
+    inter4 = ((cor2.alpha) - (cor1.alpha)) / dX;
 
-   inter1 = (abs(cor2.red) - abs(cor1.red)) / abs(dX);
-   inter2 = (abs(cor2.blue) - abs(cor1.blue)) / abs(dX);
-   inter3 = (abs(cor2.green) - abs(cor1.green)) / abs(dX);
-   inter4 = (abs(cor2.alpha) - abs(cor1.alpha)) / abs(dX);
+    for (int i = 0; i <= M; i++){
+        putPixel(A,cor1);
+        cor1.red += inter1;
+        cor1.blue += inter2;
+        cor1.green += inter3;
+        cor1.alpha += inter4;
+       
+        N += m;
 
-  
-    for (int i = 0; i <= eixoMaior; i++){
-
-        putPixel(A, cor1);
-
-      cor1.red += inter1;
-      cor1.blue += inter2;
-      cor1.green += inter3;
-      cor1.alpha += inter4;
-
-      
-
-      numerador += eixoMenor;
-
-        if (numerador > eixoMaior){
-            numerador -= eixoMaior;
-            A.x += incrementoX0;
-            A.y += incrementoY0;
-    
-
+        if (N > M){
+            N -= M;
+            A.x += x1;
+            A.y += y1;
+           //aqui tem a primeira mudança de cor do primeiro ponto   
+           //se o ponto p1 for menor que o p2
         }else {
 
-            A.x += incrementoX1;
-            A.y += incrementoY1;
-   
+            A.x += x2;
+            A.y += y2;
+           //aqui tem a primeira mudança de cor do primeiro ponto
+           //se o ponto p2 for menor que o p1  
          }
     } 
 
-      
-
 }
-
 void printLines(){
-
+  
+  
   //desenha uma linha vermelha
   p1.x = 0;
   p1.y = 0;
-  rgba1.red = 255;
-  rgba1.green = 0;
-  rgba1.blue = 0;
-  rgba1.alpha = 255;
+  p2.x = 250;
+  p2.y = 250;
+  
+  cor1.red = 255;
+  cor1.green = 0;
+  cor1.blue = 0;
+  cor1.alpha = 255;
 
-  p2.x = 511;
-  p2.y = 511;
-  rgba2.red = 0;
-  rgba2.green = 255;
-  rgba2.blue = 0;
-  rgba2.alpha = 255;
-  drawline(p1,p2,rgba1,rgba2);
+  cor2.red = 0;
+  cor2.green = 255;
+  cor2.blue = 0;
+  cor2.alpha = 255;
+  drawline(p1,p2,cor1,cor2);
   //desenha uma linha verde
-}
-  /*
-  p1.x = 425;
-  p1.y = 375;
-  rgba.red = 0;
-  rgba.green = 255;
-  rgba.blue = 0;
-  rgba.alpha = 255;
+  p3.x = 425;
+  p3.y = 375;
+  cor1.red = 0;
+  cor1.green = 255;
+  cor1.blue = 0;
+  cor1.alpha = 255;
 
-  p2.x = 350;
-  p2.y = 425;
-  rgba.red = 0;
-  rgba.green = 0;
-  rgba.blue = 255;
-  rgba.alpha = 255;
-  drawline(p1,p2,rgba1,rgba2);
+  p4.x = 350;
+  p4.y = 425;
+  cor2.red = 0;
+  cor2.green = 255;
+  cor2.blue = 0;
+  cor2.alpha = 255;
+  drawline(p3,p4,cor1,cor2);
 
   //desenha uma linha azul
-  p1.x = 200;
-  p1.y = 315;
-  rgba.red = 0;
-  rgba.green = 0;
-  rgba.blue = 255;
-  rgba.alpha = 255;
+  p5.x = 200;
+  p5.y = 315;
+  p6.x = 275;
+  p6.y = 375;
+  
+  cor1.red = 0;
+  cor1.green = 0;
+  cor1.blue = 255;
+  cor1.alpha = 255;
 
-  p2.x = 275;
-  p2.y = 375;
-  rgba.red = 0;
-  rgba.green = 255;
-  rgba.blue = 0;
-  rgba.alpha = 255;
-  drawline(p1,p2,rgba1,rgba2);
+  cor2.red = 0;
+  cor2.green = 0;
+  cor2.blue = 255;
+  cor2.alpha = 255;
+  drawline(p5,p6,cor1,cor2);
   
   
   //desenha uma linha rosa
-  p1.x = 325;
-  p1.y = 375;
-  rgba.red = 255;
-  rgba.green = 126;
-  rgba.blue = 211;
-  rgba.alpha = 255;
+  p7.x = 325;
+  p7.y = 375;
+  p8.x = 425;
+  p8.y = 315;
 
-  p2.x = 425;
-  p2.y = 315;
-  rgba.red = 255;
-  rgba.green = 0;
-  rgba.blue = 0;
-  rgba.alpha = 255;
-  drawline(p1,p2,rgba1,rgba2);
+  cor1.red = 242;
+  cor1.green = 126;
+  cor1.blue = 211;
+  cor1.alpha = 255;
+
+  cor2.red = 242;
+  cor2.green = 126;
+  cor2.blue = 211;
+  cor2.alpha = 255;
+  drawline(p7,p8,cor1,cor2);
 //desenha uma linha colorida frenética :D
   
-  p1.x = 300;
-  p1.y = 190;
-  rgba.red = 255;
-  rgba.green = 255;
-  rgba.blue = 255;
-  rgba.alpha = 255;
+ /* p9.x = 300;
+  p9.y = 190;
+  p10.x = 300;
+  p10.y = 330;
+  
+  cor1.red = rand() % 255;
+  cor1.green = rand() % 255;
+  cor1.blue = rand() % 255;
+  cor1.alpha = 255;
 
-  p2.x = 300;
-  p2.y = 330;
-  rgba.red = 255;
-  rgba.green = 255;
-  rgba.blue = 255;
-  rgba.alpha = 255;
-  drawline(p1,p2,rgba1,rgba2);
+  cor2.red = rand() % 255;
+  cor2.green = rand() % 255;
+  cor2.blue = rand() % 255;
+  cor2.alpha = 255;
+  drawline(p9,p10,cor1,cor2);*/
 
   
 //desenhar uma linha representando o chão do egito
-  p1.x = 0;
-  p1.y = 452;
-  rgba.red = 204;
-  rgba.green = 108;
-  rgba.blue = 40;
-  rgba.alpha = 255;
+  p11.x = 0;
+  p11.y = 452;
+  p12.x = 512;
+  p12.y = 452;
+  
+  cor1.red = 204;
+  cor1.green = 108;
+  cor1.blue = 40;
+  cor1.alpha = 255;
 
-  p2.x = 512;
-  p2.y = 452;
-  rgba.red = 204;
-  rgba.green = 108;
-  rgba.blue = 40;
-  rgba.alpha = 255;
-  drawline(p1,p2,rgba,rgba2);
-  
-  
+  cor2.red = 204;
+  cor2.green = 108;
+  cor2.blue = 40;
+  cor2.alpha = 255;
+  drawline(p11,p12,cor1,cor2);
 }      
-void drawTriagle(Pixel p1, Pixel p2, Pixel p3)
+void drawTriagle(Pixel A, Pixel B, Pixel C)
 {
-  drawline(p1,p2,rgba1,rgba2);
-  drawline(p2,p3,rgba1,rgba2);
-  drawline(p1,p3,rgba1,rgba2);
+  drawline(A,B,cor1,cor2);
+  drawline(B,C,cor1,cor2);
+  drawline(A,C,cor1,cor2);
 } 
 
 void printTriagle(){
   //desenha um triangulo piscante
   p1.x = 250;
   p1.y = 450;
-  rgba.red = 255;
-  rgba.green = 255;
-  rgba.blue = 255;
-  rgba.alpha = 255;
-
   p2.x = 300;
   p2.y = 350;
-  rgba.red = 255;
-  rgba.green = 255;
-  rgba.blue = 255;
-  rgba.alpha = 255;
-
   p3.x = 350;
   p3.y = 450;
-  rgba.red = 255;
-  rgba.green = 255;
-  rgba.blue = 255;
-  rgba.alpha = 255;
+  
+  cor1.red = 255;
+  cor1.green = 255;
+  cor1.blue = 0;
+  cor1.alpha = 255;
+  
+  cor1.red = 255;
+  cor1.green = 255;
+  cor1.blue = 0;
+  cor1.alpha = 255;
+
+  
 
   drawTriagle(p1,p2,p3);
   //desenha um triangulo com animação randomica de todos os pontos
@@ -361,28 +322,24 @@ void printTriagle(){
   p3x = 305 -(rand() % 10);
   p3y = 410 -(rand() % 10);
     
-  p1.x = p1x;
-  p1.y = p1y;
-  rgba.red = rand() % 255;
-  rgba.green = rand() % 255;
-  rgba.blue = rand() % 255;
-  rgba.alpha = rand() % 255;
+  p4.x = p1x;
+  p4.y = p1y;
+  p5.x = p2x;
+  p5.y = p2y;
+  p6.x = p3x;
+  p6.y = p3y;
   
-  p2.x = p2x;
-  p2.y = p2y;
-  rgba.red = rand() % 255;
-  rgba.green = rand() % 255;
-  rgba.blue = rand() % 255;
-  rgba.alpha = rand() % 255;
- 
-  p3.x = p3x;
-  p3.y = p3y;
-  rgba.red = rand() % 255;
-  rgba.green = rand() % 255;
-  rgba.blue = rand() % 255;
-  rgba.alpha = rand() % 255;
+  cor1.red = rand() % 255;
+  cor1.green = rand() % 255;
+  cor1.blue = rand() % 255;
+  cor1.alpha = rand() % 255;
+  
+  cor2.red = 0;
+  cor2.green = 5;
+  cor2.blue = 255;
+  cor2.alpha = 255;
+  drawTriagle(p4,p5,p6);
 
-  drawTriagle(p1,p2,p3);
-  
-}    
-*/
+}
+
+
